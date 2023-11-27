669714c4ebc8bdda2324fbad87ed837cde7c9d51
Mohamed Bekdach <75533061+BigLiban@users.noreply.github.com>
2023-11-27 00:09:27 -0500

    finished salary pdf reading

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-variable_bob = "hello"
+# pylint: disable=missing-module-docstring
+import tabula
 
-print(f"hello world {variable_bob}")
+pdf_path = "./pdf_files/salaries.pdf"
+
+tables: list = tabula.read_pdf(pdf_path, pages='1-458')
+
+total = 0
+times_run = 0
+
+for table in tables:
+    for _, row in table.iterrows():
+        for item in row:
+            if isinstance(item, str) and "." in item:
+                total += float(item.replace(',', ''))
+                times_run += 1
+
+print(f"This is the total: {total}")
+print(f"times run: {times_run}")
